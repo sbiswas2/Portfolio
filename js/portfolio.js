c32e6c5b02053b596a190c219f5b5e960a343f46
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2018-01-30 11:15:08 -0600

    updated portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,116 @@
+// ==================
+// PROFILE SLIDE SHOW
+// ==================
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var photos = document.getElementsByClassName("profile_photo");
+    for (i = 0; i < photos.length; i++) {
+      photos[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > photos.length) {slideIndex = 1} 
+    photos[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 9000); // Change image every 10 seconds
+}
+
+// =====================
+// SKILLS PHOTO SCROLLER
+// =====================
+$(function(){
+        var scroller = $('#scroller div.innerScrollArea');
+        var scrollerContent = scroller.children('ul');
+        scrollerContent.children().clone().appendTo(scrollerContent);
+        var curX = 0;
+        scrollerContent.children().each(function(){
+            var $this = $(this);
+            $this.css('left', curX);
+            curX += $this.outerWidth(true);
+        });
+        var fullW = curX / 2;
+        var viewportW = scroller.width();
+
+        // Scrolling speed management
+        var controller = {curSpeed:0, fullSpeed:2};
+        var $controller = $(controller);
+        var tweenToNewSpeed = function(newSpeed, duration)
+        {
+            if (duration === undefined)
+                duration = 600;
+            $controller.stop(true).animate({curSpeed:newSpeed}, duration);
+        };
+
+        // Pause on hover
+        scroller.hover(function(){
+            tweenToNewSpeed(0);
+        }, function(){
+            tweenToNewSpeed(controller.fullSpeed);
+        });
+
+        // Scrolling management; start the automatical scrolling
+        var doScroll = function()
+        {
+            var curX = scroller.scrollLeft();
+            var newX = curX + controller.curSpeed;
+            if (newX > fullW*2 - viewportW)
+                newX -= fullW;
+            scroller.scrollLeft(newX);
+        };
+        setInterval(doScroll, 20);
+        tweenToNewSpeed(controller.fullSpeed);
+    });
+
+// ==========================
+// FANCY BOX CONTENT CREATION
+// ==========================
+$(".fancybox").fancybox({
+    openEffect  : 'none',
+    closeEffect : 'none',
+    loop: true,
+    afterLoad   : function() {
+        this.content.html();
+    }
+});
+
+// ============================
+// DIVS EQUAL HEIGHTS ON RESIZE
+// ============================
+equalheight = function(container){
+
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(container).each(function() {
+
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+}
+
+$(window).load(function() {
+  equalheight('.single-service');
+});
+
+$(window).resize(function(){
+  equalheight('.single-service');
+});
